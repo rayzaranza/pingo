ace99df177fe5c34598bda25a4c672adfcc3e9ce
rayzaranza <rayzaranza@gmail.com>
2026-05-09 23:57:49 -0300

    feat: add pingo theme to storybook

---
+++ .storybook/PingoTheme.ts
@@ -0,0 +1,20 @@
+import { create } from "storybook/theming/create";
+import { fontFamily, semantic } from "../src/styles/tokens.ts";
+
+const bg = semantic.color.background.default;
+
+export default create({
+  base: "light",
+  brandTitle: "<h1><b>Pingo</b></h1>",
+  fontBase: fontFamily,
+  appBg: bg,
+  appPreviewBg: bg,
+  appContentBg: bg,
+  barBg: bg,
+  inputBg: bg,
+  buttonBg: bg,
+  textColor: semantic.color.content.default,
+  appHoverBg: semantic.color.background.hover,
+  colorPrimary: semantic.color.background.accent,
+  textMutedColor: semantic.color.content.secondary,
+});
