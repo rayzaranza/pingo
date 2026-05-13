078fb950b604f60442182b6a283668c4f7774bfa
rayzaranza <rayzaranza@gmail.com>
2026-05-13 16:58:09 -0300

    feat: add icons for alert component

---
+++ src/components/Icon/glyphs/CheckCircle.tsx
@@ -0,0 +1,4 @@
+// Auto-generated — do not edit manually.
+export function CheckCircle() {
+  return <path d="m6.9 12.087 2.664 2.663a1.01 1.01 0 0 0 1.433 0l5.367-5.368M12 21.5a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19"/>
+}
