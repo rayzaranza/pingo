7925ad4f09cdf389ff7f6ec175ce6e7ad3fcff0e
rayzaranza <rayzaranza@gmail.com>
2026-05-20 14:06:37 -0300

    feat: add alert, input and card to export

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,6 @@ import "@/styles/main.css";
 export { Button } from "@/components/Button/Button";
 export { Icon } from "@/components/Icon/Icon";
 export { Text } from "@/components/Text/Text";
+export { Alert } from "@/components/Alert/Alert";
+export { Input } from "@/components/Input/Input";
+export { Card } from "@/components/Card/Card";
