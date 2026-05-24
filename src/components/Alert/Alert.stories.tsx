88d81c25ddc67f4422fd4109f1206617c566aed9
rayzaranza <rayzaranza@gmail.com>
2026-05-24 17:07:12 -0300

    feat: add inline prop

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -3,30 +3,38 @@ import { Alert } from "./Alert";
 
 const meta = preview.meta({ component: Alert });
 
-export const AlertSuccess = meta.story({
-  args: {
-    children: "This is email is available",
-    variant: "success",
-  },
-});
-
-export const AlertDanger = meta.story({
+export const Error = meta.story({
   args: {
     children: "Email is required",
     variant: "error",
   },
 });
 
-export const AlertWarning = meta.story({
+export const Success = meta.story({
+  args: {
+    children: "This is email is available",
+    variant: "success",
+  },
+});
+
+export const Warning = meta.story({
   args: {
     children: "Your email is not verified yet",
     variant: "warning",
   },
 });
 
-export const AlertInfo = meta.story({
+export const Info = meta.story({
   args: {
     children: "Asset list were moved to your profile",
     variant: "info",
+  },
+});
+
+export const Inline = meta.story({
+  args: {
+    children: "Email is required",
+    variant: "error",
+    inline: true,
   },
 });
